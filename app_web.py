26d27db70a7336048f2e7f300b943a56d47cfa99
klaus3411 <lgrauaguirre@gmail.com>
2026-07-08 09:40:45 -0500

    Add files via upload

--- app_web.py
+++ app_web.py
@@ -12,7 +12,7 @@ import streamlit.components.v1 as components
 st.set_page_config(page_title="Portal Educativo - Alternancia", page_icon="🏫", layout="centered")
 
 # --- VARIABLE DE BRANDING (Pon aquí el link de tu logo) ---
-URL_LOGO_COLEGIO = "https://images.leadconnectorhq.com/image/f_webp/q_80/r_1200/u_https://assets.cdn.filesafe.space/CqwkTEZZmpZXlnX4iVPm/media/7da16945-4101-474d-8992-326abe631293.png" 
+URL_LOGO_COLEGIO = "https://scontent.fctg2-1.fna.fbcdn.net/v/t39.30808-6/377432631_785816130218699_8439928282516365142_n.jpg?stp=dst-jpg_tt6&cstp=mx1080x1080&ctp=s1080x1080&_nc_cat=111&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=QkM_KdoM6-8Q7kNvwHZLafy&_nc_oc=Ado8PMT8UfZ5Na3A5PElaVATxsfC2uGumMesGdurEkH6giRzWqSz1reFRnxInqrbSwo&_nc_zt=23&_nc_ht=scontent.fctg2-1.fna&_nc_gid=8GpDrRfgp9dp29bAVZcYkg&_nc_ss=7b289&oh=00_AQA17yO0TLEY20eEhXYhcluOHLBzxi2TEmATg-_PeChiTg&oe=6A544484" 
 
 # ==========================================
 # 1.5 INYECCIÓN PWA (App Instalable para Celulares)
